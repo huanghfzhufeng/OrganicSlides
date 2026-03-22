258062cfededea50be35ba656f29f228a90e3438
huanghfzhufeng <2461646599@qq.com>
2026-03-23 04:26:16 +0800

    Cleanup dead frontend assets

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -5,7 +5,6 @@ export { default as seedlingIcon } from './seedling.png';
 export { default as zenStonesIcon } from './zen-stones.png';
 export { default as flowingLeavesIcon } from './flowing-leaves.png';
 export { default as mushroomStumpIcon } from './mushroom-stump.png';
-export { default as forestGlobeIcon } from './forest-globe.png';
 export { default as intertwinedRootsIcon } from './intertwined-roots.png';
 export { default as dandelionIcon } from './dandelion.png';
 export { default as organicGearsIcon } from './organic-gears.png';
